ab74c55b953130da659195ac783fa21573cccc96
Will Folsom <folsomwg@gmail.com>
2020-11-29 15:07:40 -0500

    Complete Koans

--- about_files.go
+++ about_files.go
@@ -3,9 +3,9 @@ package go_koans
 import "io/ioutil"
 import "strings"
 
-func aboutFiles() {
+func aboutFiles() { //5
 	filename := "about_files.go"
 	contents, _ := ioutil.ReadFile(filename)
 	lines := strings.Split(string(contents), "\n")
-	assert(lines[5] == __string__) // handling files is too trivial
+	assert(lines[5] == "func aboutFiles() { //5") // handling files is too trivial
 }
